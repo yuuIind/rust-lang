8a84cbac0ba6fc03aa794bf2646885774ea9dec0
Hakan Taştan <mhakan.tastan@gmail.com>
2024-03-14 23:06:14 +0300

    Delete book/temperature_converter/target directory

--- book/temperature_converter/target/debug/temperature_converter.d
+++
@@ -1,1 +0,0 @@
-C:\Users\hakan\OneDrive\Masaüstü\Rust\rust_lang\temperature_converter\target\debug\temperature_converter.exe: C:\Users\hakan\OneDrive\Masaüstü\Rust\rust_lang\temperature_converter\src\main.rs
