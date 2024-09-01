c51ebdef3fac2ab73a745550a0ec500693924ae5
yuulind <mhakan.tastan@gmail.com>
2024-09-01 12:54:14 +0300

    Refactor Fibonacci number calculation for improved performance

--- book/fibonacci_number/src/main.rs
+++ book/fibonacci_number/src/main.rs
@@ -1,49 +1,85 @@
 use std::io;
 
 fn main() {
-    println!("Welcome!");
+    println!("Hey, There!👋");
+    println!("Which Fibonacci number do you want to know?");
+    println!("Please enter n:");
+    
+    let n: u32 = loop {
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).expect("Failed to read line");
 
-    loop {
-        println!("Please input the number of the fibonacci sequence.");
-        println!("Type 'exit' to quit.");
-
-        let mut n = String::new();
-
-        io::stdin()
-            .read_line(&mut n)
-            .expect("Failed to read line");
-
-        let n: u32 = match n.trim().parse() {
-            Ok(num) => num,
-            Err(_) => {
-                if n.to_lowercase().trim() == "exit" {
-                    println!("Exiting the program...");
-                    break;
-                } else {
-                    println!("Please type a valid number! e.g. a positive integer\n");
-                    continue;
-                }
-            }
+        match input.trim().parse() {
+            Ok(num) if num > 0 => break num,
+            _ => {
+                println!("Please type a positive integer greater than 0!");
+                continue;
+            },
         };
-        
-        println!("The fibonacci number is: {}", fibonacci(n));
+    };
+    
+    println!("Ah! Just one more thing,");
+    println!("Would you like to find it through wisdom or perseverance");
+
+    let choice: String = loop {
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).expect("Failed to read line");
+
+        match input.trim().to_lowercase().as_str() {
+            "wisdom" | "perseverance" => break input.trim().to_string(),
+            _ => {
+                println!("Invalid choice. Please type 'wisdom' or 'perseverance'.");
+                continue;
+            },
+        };
+    };
+
+    let fib_n = fibonacci(n, &choice);
+    println!("Fibonacci number {} is {}!", n, fib_n); 
+}
+
+fn fibonacci(n: u32, choice: &str) -> u64 {
+    if choice == "wisdom" {
+        fibonacci_closed(n as i32)
+    } else {
+        fibonacci_memoization(n)
     }
 }
 
-fn fibonacci(n: u32) -> u32 {
-    if n == 0 {
-        return 0;
-    } else if n == 1 {
-        return 1;
-    } else {
-        let mut sum = 0;
-        let mut current = 1;
-        let mut last = 0;
-        for _i in 1..n {
-            sum = current + last;
-            last = current;
-            current = sum;
+fn fibonacci_closed(n: i32) -> u64 {
+    let phi: f64 = (1.0 + (5.0f64).sqrt()) / 2.0;
+    let psi: f64 = (1.0 - (5.0f64).sqrt()) / 2.0;
+
+    // Use Binet's formula
+    let fib_n = (phi.powi(n) - psi.powi(n)) / (5.0f64).sqrt();
+
+    // turn into an integer by rounding and converting u32
+    fib_n.round() as u64
+}
+
+fn fibonacci_memoization(n: u32) -> u64 {
+    // Initialize the memoization vector with a size of n + 1
+    let mut memo: Vec<u64> = vec![0; (n + 1) as usize];
+    memo[1] = 1;
+
+    // The function that performs the actual calculation
+    fn fib_gen(n: u32, memo: &mut Vec<u64>) -> u64 {
+        if n == 0 {
+            return 0;
+        } else if n == 1 {
+            return 1;
         }
-        sum
+
+        // Check if the value is already computed
+        if memo[n as usize] != 0 {
+            return memo[n as usize];
+        }
+
+        // Otherwise, Compute and store the value
+        memo[n as usize] = fib_gen(n - 1, memo) + fib_gen(n - 2, memo);
+        memo[n as usize]
     }
+
+    // Call the helper function
+    fib_gen(n, &mut memo)
 }
